9e49d92682cd12893eee938f114aec22bac16493
jinri1218 <jinri.1218@yahoo.com>
2020-10-28 18:08:50 +0800

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,120 @@
+<template>
+    <div>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="">
+                    <img src="/images/logo.png" alt="logo">
+                    <span>PT Tekno Innova Solusindo</span>
+                </a>
+                <div class="collapse navbar-collapse">
+                    <ul class="navbar-nav">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="">
+                                HOME
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="">
+                                TENTANG KAMI
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="">
+                                PRODUK KAMI
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="">
+                                BANTUAN
+                            </a>
+                        </li>
+                    </ul>
+                    <a class="btn btn-contact" href="">
+                        Hubungi Kami
+                    </a>
+                </div>
+            </div>
+        </nav>
+        <Nuxt/>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.navbar {
+    padding-bottom: 0;
+    height: 75px;
+}
+
+.bg-light {
+    box-shadow: 0 6px 3px 0 rgba(155, 155, 155, 0.86);
+    background-color: #f3f3f3;
+}
+
+.navbar-brand {
+    padding-bottom: 0;
+    padding-top: 0;
+    white-space: nowrap;
+    font-weight: bold;
+
+    img {
+        height: 70px;
+    }
+
+    span {
+        vertical-align: middle;
+    }
+}
+
+@media (min-width: 992px) {
+    .navbar-brand {
+        margin-left: 126px;
+        margin-right: 218px;
+    }
+}
+
+.nav-item {
+    position: relative;
+
+    .nav-link {
+        white-space: nowrap;
+        font-weight: 600;
+        line-height: 60px;
+        color: #444446;
+        height: 100%;
+        padding-left: 20px;
+        padding-right: 20px;
+    }
+
+    &.active {
+        .nav-link {
+            color: #70a8e5;
+        }
+    }
+
+    &.active:after {
+        content: "";
+        position: absolute;
+        left: 50%;
+        bottom: -10px;
+        transform: translate(-50%, -50%);
+        background-color: #70a8e5;
+        width: 14px;
+        height: 14px;
+        border-radius: 50%;
+    }
+}
+
+.btn-contact {
+    margin-left: auto;
+    margin-right: 33px;
+    opacity: 0.9;
+    border-radius: 30px;
+    box-shadow: 6px 1px 16px 0 rgba(0, 0, 0, 0.17);
+    background-image: linear-gradient(281deg, #f4f4f4 0, #e6e6e6 100%);
+
+    color: #70a8e5;
+    white-space: nowrap;
+    padding-left: 40px;
+    padding-right: 40px;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,124 @@
+<template>
+    <div class="main-content">
+        <section class="section-1 text-center">
+            <img class="bg" src="/images/1.png" alt="">
+            <img class="slogan" src="/images/slogan.png" alt="">
+            <a href="#" class="btn btn-blue"></a>
+        </section>
+
+        <section class="section-2">
+            <div class="row">
+                <div class="col-6">
+                    <h3>
+                        We work to <br> make <span class="text-blue">impact</span>
+                    </h3>
+                    <p>
+                        Era digital seharusnya menyejahterakan banyak orang,<br>
+                        bukan hanya segelintir orang. Kami akan terus menciptakan<br>
+                        produk-produk digital yang inovatif untuk Indonesia sejahtera.
+                    </p>
+                    <div>
+                        <a href="#" class="btn btn-blue border-radius-30">Hubungi Kami</a>
+                    </div>
+                </div>
+                <div class="col-6">
+
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-6">
+
+                </div>
+                <div class="col-6">
+
+                </div>
+            </div>
+        </section>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Index',
+    head: function () {
+        return {
+            title: 'Home'
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.main-content {
+    padding-top: 75px;
+    color: #707070;
+
+    section {
+        padding-left: 150px;
+        padding-right: 150px;
+    }
+}
+
+.section-1 {
+    background-image: url("/images/bg1.jpg");
+    position: relative;
+    padding-top: 82px;
+
+    img {
+        &.bg {
+            width: 100%;
+        }
+
+        &.slogan {
+            position: absolute;
+            top: 35%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            width: 30%;
+        }
+    }
+
+    .btn-blue {
+        width: 250px;
+        height: 45px;
+        opacity: 0.59;
+        border-radius: 30px;
+        box-shadow: 0 8px 6px 0 rgba(0, 0, 0, 0.16);
+        background-color: #70a8e5;
+
+        position: absolute;
+        left: 50%;
+        bottom: 8%;
+        transform: translate(-50%, -50%);
+    }
+}
+
+.section-2 {
+    padding-top: 100px;
+    padding-bottom: 100px;
+
+    h3 {
+        font-size: 32px;
+        font-weight: bold;
+        line-height: 1.33;
+        color: #707070;
+        margin-top: 30px;
+        margin-bottom: 30px;
+    }
+
+    p {
+        font-size: 18px;
+        line-height: 1.5;
+        letter-spacing: 0.25px;
+        text-align: left;
+        margin-top: 30px;
+        margin-bottom: 50px;
+    }
+
+    .btn {
+        padding-left: 30px;
+        padding-right: 30px;
+    }
+}
+
+</style>
